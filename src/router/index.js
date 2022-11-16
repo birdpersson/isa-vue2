bdfbf49f590e5926c56fc2ba1ad250fa8fc554ce
birdpersson <aleksandarvujasin@gmail.com>
2022-11-16 17:34:53 +0100

    add centers

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Signin from "../views/Signin.vue";
 import Signup from "../views/Signup.vue";
+import Centers from "../views/Centers.vue";
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/signup",
     name: "Signup",
     component: Signup
+  },
+  {
+    path: "/center/",
+    name: "Centers",
+    component: Centers
   },
   {
     path: '/about',
---
+++ src/views/Centers.vue
@@ -0,0 +1,57 @@
+<script>
+import axios from "axios";
+export default {
+  name: "Centers",
+  data() {
+    return {
+      centers: [],
+    };
+  },
+  methods: {
+    reserve: function (id) {
+      this.$router.push("/questionnaire/");
+    },
+  },
+  created() {
+    axios.get("http://localhost:8080/center/").then((Response) => {
+      console.log(Response.data);
+      this.centers = Response.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <div id="centers">
+    <div class="card-body">
+      <div class="card-header">
+        <h4>Centers</h4>
+      </div>
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>City</th>
+            <th>Address</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr :key="center.id" v-for="center in centers">
+            <td>{{ center.name }}</td>
+            <td>{{ center.city }}</td>
+            <td>{{ center.address }}</td>
+            <td>{{ center.average }}</td>
+            <td>
+              <b-button @click="reserve(center.id)">Reserve</b-button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
